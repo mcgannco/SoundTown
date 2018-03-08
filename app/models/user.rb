77272152dcc31900bb217897dc639747ec161d1d
Connor <mcgann.27@buckeyemail.osu.edu>
2018-03-07 21:12:33 -0500

    validates uniqness on usermodel for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   validates :username, :password_digest, :session_token, presence: true
-
+  validates :username, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
   attr_reader :password
