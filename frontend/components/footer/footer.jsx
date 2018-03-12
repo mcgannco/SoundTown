4056495f35edc65fe36797b3f4d7da435e046ebe
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-03-11 21:34:22 -0400

    style

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,8 +5,17 @@ const Footer = () => {
   return(
     <div className="FooterContainer">
       <div className="PlayBar">
-        <audio className="audiobar" controls type="audio/ogg">
-        </audio>
+        <button className="playButton"></button>
+        <div className="songbar">
+          <div className="Duration">0:00
+          </div>
+          <div className="slider">
+            <input type="range" step="any" min="0" max="1" className="progress"></input>
+            <div className="slider-track">
+            </div>
+            <div className=="progress"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
