e874beaed68c4815158945ff375fcc231e6d3eed
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-03-27 13:03:26 -0400

    style

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ module SoundTown
     config.load_defaults 5.1
 
     config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
+        :storage => :s3,
+        :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
